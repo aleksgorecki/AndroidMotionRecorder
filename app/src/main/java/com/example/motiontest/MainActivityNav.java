03c6c0fb116d5e0963f5b0e46728f36e41d8be87
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-12-10 20:20:12 +0100

    added motion recording to main MainActivityNav

--- app/src/main/java/com/example/motiontest/MainActivityNav.java
+++ app/src/main/java/com/example/motiontest/MainActivityNav.java
@@ -1,7 +1,20 @@
 package com.example.motiontest;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +24,32 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.motiontest.databinding.ActivityMainNavBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivityNav extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivityNav extends AppCompatActivity implements SensorEventListener {
 
     private ActivityMainNavBinding binding;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private int sampleNo = 0;
+    private boolean isRecording = false;
+    private final int motionDurationMs = 700;
+    private final float maxY = 25;
+    private final float minY = -25;
+    private final int xColor = Color.RED;
+    private final int yColor = Color.GREEN;
+    private final int zColor = Color.BLUE;
+
+    private final ArrayList<Entry> xData = new ArrayList<>();
+    private final ArrayList<Entry> yData = new ArrayList<>();
+    private final ArrayList<Entry> zData = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +66,135 @@ public class MainActivityNav extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_nav);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        initChartConfiguration();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        binding.fabRecord.setOnClickListener(view -> startRecording());
+
     }
 
+    private void initChartConfiguration() {
+
+        LineChart mainChart = binding.mainChart;
+
+        mainChart.setDrawGridBackground(false);
+        mainChart.setNoDataText("Brak wykresu");
+        mainChart.getAxisRight().setEnabled(false);
+        mainChart.setVisibleYRange(minY, maxY, YAxis.AxisDependency.LEFT);
+        mainChart.setAutoScaleMinMaxEnabled(false);
+        mainChart.getDescription().setEnabled(false);
+
+        XAxis xaxis = mainChart.getXAxis();
+        YAxis yaxis = mainChart.getAxisLeft();
+
+        xaxis.setDrawAxisLine(false);
+        xaxis.setDrawGridLines(false);
+
+        yaxis.setDrawGridLines(false);
+        yaxis.setAxisMinimum(minY);
+        yaxis.setAxisMaximum(maxY);
+
+        switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                xaxis.setTextColor(Color.WHITE);
+                yaxis.setTextColor(Color.WHITE);
+                mainChart.getLegend().setTextColor(Color.WHITE);
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+                xaxis.setTextColor(Color.BLACK);
+                yaxis.setTextColor(Color.BLACK);
+                mainChart.getLegend().setTextColor(Color.BLACK);
+                break;
+        }
+    }
+
+    private void clearRecordedData() {
+        xData.clear();
+        yData.clear();
+        zData.clear();
+        sampleNo = 0;
+    }
+
+    private void drawRecordedDataOnChart() {
+
+        LineChart mainChart = binding.mainChart;
+
+        LineDataSet xDataSet = new LineDataSet(xData, "X");
+        LineDataSet yDataSet = new LineDataSet(yData, "Y");
+        LineDataSet zDataSet = new LineDataSet(zData, "Z");
+
+        styleDataSets(xDataSet, yDataSet, zDataSet);
+
+        LineData combinedData = new LineData(xDataSet, yDataSet, zDataSet);
+        mainChart.setAutoScaleMinMaxEnabled(true);
+        mainChart.setData(combinedData);
+        mainChart.notifyDataSetChanged();
+        mainChart.invalidate();
+    }
+
+    private void styleDataSets(LineDataSet xDataSet, LineDataSet yDataSet, LineDataSet zDataSet) {
+        xDataSet.setColor(xColor);
+        yDataSet.setColor(yColor);
+        zDataSet.setColor(zColor);
+
+        LineDataSet[] dataSets = {xDataSet, yDataSet, zDataSet};
+        for (LineDataSet dataSet: dataSets) {
+            dataSet.setDrawValues(false);
+            dataSet.setCircleColor(dataSet.getColor());
+            dataSet.setCircleHoleColor(dataSet.getColor());
+        }
+    }
+
+    private void startRecording() {
+
+        FloatingActionButton recordButton = binding.fabRecord;
+
+        clearRecordedData();
+        isRecording = true;
+        recordButton.setEnabled(false);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(() -> {
+                    isRecording = false;
+                    drawRecordedDataOnChart();
+                    recordButton.setEnabled(true);
+                });
+            }
+        }, motionDurationMs);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) { }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        if (isRecording) {
+            xData.add(new Entry(sampleNo, x));
+            yData.add(new Entry(sampleNo, y));
+            zData.add(new Entry(sampleNo, z));
+
+            sampleNo++;
+        }
+    }
 
 }
