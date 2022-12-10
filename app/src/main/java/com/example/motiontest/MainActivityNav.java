d09e23aca916efa2da565eb9ddd6e0ae1fea7023
Aleksander Górecki <goreckialeksander9@gmail.com>
2022-12-10 17:18:17 +0100

    added fragments and icons

--- app/src/main/java/com/example/motiontest/MainActivityNav.java
+++ app/src/main/java/com/example/motiontest/MainActivityNav.java
@@ -26,12 +26,12 @@ public class MainActivityNav extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_dataset, R.id.navigation_testing, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_nav);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
 
 }
